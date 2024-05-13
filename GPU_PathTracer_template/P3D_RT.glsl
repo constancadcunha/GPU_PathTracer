0158076be140731fabc136fbff4063317be8c446
Sofia Pinho <sofia.pinho@tecnico.ulisboa.pt>
2024-05-13 11:15:07 +0100

    chore: change focal point for dof

--- GPU_PathTracer_template/P3D_RT.glsl
+++ GPU_PathTracer_template/P3D_RT.glsl
@@ -236,8 +236,8 @@ void main()
     vec3 camPos = vec3(mouse.x * 10.0, mouse.y * 5.0, 8.0);
     vec3 camTarget = vec3(0.0, 0.0, -1.0);
     float fovy = 60.0;
-    float aperture = 20.0;
-    float distToFocus = 1.5;
+    float aperture = 50.0;
+    float distToFocus = 0.3;
     float time0 = 0.0;
     float time1 = 1.0;
     Camera cam = createCamera(
